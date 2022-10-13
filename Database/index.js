29fef7bf62ab6efdbadac6ad867b44eb2c5baaf6
Atharva Tribhuwane <100659665+atharva-tribhuwane@users.noreply.github.com>
2022-10-13 17:07:16 +0530

    Hosted db on Atlas Final final

--- Database/index.js
+++ Database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const ConnectDatabase = ()=>{
     return new Promise((resolve,reject)=>{
-        mongoose.connect("mongodb://localhost:27017/5C",(err)=>{
+        mongoose.connect("mongodb+srv://tribhuwanea:sampledb123@cluster0.dsidffs.mongodb.net/5C?retryWrites=true&w=majority",(err)=>{
             if(err){
                 console.log("Error Connecting Database", err);
                 reject();
